9c5d7c09abe05f3e892e3a219eb273671b5a9e7d
goffette09 <b94703017@gmail.com>
2023-12-16 14:09:08 +0800

    modify

--- pico_w/Pico10_1.py
+++ pico_w/Pico10_1.py
@@ -1,9 +1,10 @@
 from machine import ADC,Timer
 
 def callback1(t:Timer):
-    temperature = ADC(4) 
+    sensor = ADC(4) # the 5th pin (0~4)
     #print(temperature.read_u16())
-    vol = temperature.read_u16()*3.3/65535
-    print(vol)
+    vol = sensor.read_u16()*3.3/65535
+    temperature = 27 - (vol-0.706)/0.001721
+    print(temperature)#vol) 
 time1 =Timer()
 time1.init(period=1000, callback=callback1)
