4d6d95e1cb027086b2a32967a5789adf44cb6f98
goffette09 <b94703017@gmail.com>
2024-01-20 11:46:17 +0800

    modify

--- pico_w/Pico13_2.py
+++ pico_w/Pico13_2.py
@@ -5,5 +5,9 @@ green_led = Pin(15, mode=Pin.OUT) # Pin(15,Pin.OUT)亦可
 button= Pin(14, mode=Pin.PULL_DOWN) #設定GPIO14為button下拉電阻
 
 while True:
-    print(button.value())
-    time.sleep_ms(500)# sleep for 500 milliseconds (0.5秒)
+    if button.value() == 1:
+        #print("按下")
+        green_led.value(1)
+    else:
+        green_led.value(0)
+        
