39785c3025ff5c0100deef27446c57bdaa3687d0
goffette09 <151415018+goffette09@users.noreply.github.com>
2024-02-03 03:18:16 +0000

    modify

---
+++ GUI/main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Pico_W專案')
+st.header("雞舍:orange[溫度]和:rainbow[光線]狀態")
+st.divider()
+
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [23.9, 120.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
+
+#st.snow()
+
+#sample_rate = 44100  # 44100 samples per second
+#seconds = 2  # Note duration of 2 seconds
+
+#frequency_la = 440  # Our played note will be 440 Hz
+
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+#t = np.linspace(0, seconds, seconds * sample_rate, False)
+
+# Generate a 440 Hz sine wave
+#note_la = np.sin(frequency_la * t * 2 * np.pi)
+#st.audio(note_la, sample_rate=sample_rate)
+
+
+
+
