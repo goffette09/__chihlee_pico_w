1e0ca8de8a94d3cf6682285715a5b0b47758260c
goffette09 <151415018+goffette09@users.noreply.github.com>
2024-02-03 06:18:15 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -1,11 +1,21 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import requests
 
 st.title('Pico_W專案:更新')
 st.header("雞舍:orange[溫度]和:rainbow[光線]狀態")
 st.divider()
 
+url = 'https://blynk.cloud/external/api/get?token=gg8uB11T8EXSWdrm5XJvc6sTKM5jdMPF&v0&v1'
+
+response = requests.request("GET", url)
+if response.status_code == 200:
+    all_data= response.json()
+    st.info(f'光線:{all_data["v0"]}') #字串差補(數轉文):f'{}'
+    st.warning(f'可變電阻:{all_data["v1"]}')
+else:
+    st.write("連線失敗,請等一下再試。") 
 
 df = pd.DataFrame(
     np.random.randn(100, 2) / [50, 50] + [23.487614, 120.959522],
